e48005308394018697306b1070dcc4322e53da31
Isac <aristocrata.black@gmail.com>
2026-05-04 17:55:48 -0300

    first commit

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.onload = function () {
 const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
-    const question = item.querySelector('.faq-question');
+    const question = item.querySelector('.faq-q');
     question.addEventListener('click', () => {
         item.classList.toggle('active');
     });
